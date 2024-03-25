a1e50e5cbf5620d9cd174c1b275fcbc3b5c6ad45
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-25 12:16:48 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-let unorderValue = document.querySelector("#band");
+let unorderedList = document.querySelector("#band");
+
+function customSort(a, b) {
+    const ignoreWord = ['a', 'an', 'the'];
+    const getTitle = (band) => band.replace(/^(the |an |a )/i, '');
+    const titleA = getTitle(a);
+    const titleB = getTitle(b);
+
+    return titleA.localeCompare(titleB);
+}
+
+bands.sort(customSort);
+
+bands.forEach((element) => {
+    let listItem = document.createElement('li');
+    listItem.textContent = element;
+    unorderedList.appendChild(listItem);
+});

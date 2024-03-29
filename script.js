ff86a33374c33c9f43572cfd471011a48e78f546
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-29 12:15:36 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-let unorderedList = document.querySelector("#band");
+let unorderedList = document.querySelector("#bands");
 
 function customSort(a, b) {
     const ignoreWord = ['a', 'an', 'the'];
